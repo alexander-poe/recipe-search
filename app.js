40519d888d96ffa294574f5a683b0009bce42bb7
Theresa Wilkin <theresawilkin@yahoo.com>
2016-11-10 12:54:37 -0500

    first working version

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-var url = 'http://crossorigin.me/https://api.edamam.com/search';
-
-var searchObj = {
-	q:'',
-	app_id: '6da7e93e',
-	app_key: '8e6eaad81cc090bc90cb3479ffff21f6'
-
+var state = {
+	url: 'http://crossorigin.me/https://api.edamam.com/search',
+	query: '',
+	searchObj: {
+		q:'',
+		app_id: '6da7e93e',
+		app_key: '8e6eaad81cc090bc90cb3479ffff21f6'
+	}
 }
 
 $(function() {
 	function newSearch(query) {
-		searchObj.q = query; 
-		return $.getJSON(url, searchObj, function(data) {
-			console.log('works');
-		})
+		state.searchObj.q = state.query; 
+		$.getJSON(state.url, state.searchObj, renderData);
 	};
-	newSearch('cookies');
+
+	function renderData(data) {
+		var resultsHtml = ''; 
+		data.hits.forEach(function(result) {
+			var img = result.recipe.image;
+			var link = result.recipe.url;
+			var recipeHtml = '<a href="' + link + '"><img src="' + img + 
+			'"></a>';
+			resultsHtml += recipeHtml;
+		})
+		$("#results").html(resultsHtml);
+	};
+
+$("form").submit(function(e) {
+	e.preventDefault();
+	state.query = $("#searchRecipes").val();
+	newSearch(state.query);
+})
+
+
 });
 
--- recipe.md
+++ recipe.md
@@ -1,12 +1,12 @@
 https://api.edamam.com/search?q=chicken&app_id=6da7e93e&app_key=8e6eaad81cc090bc90cb3479ffff21f6
 
 
-user enters keyword
-user clicks submit
-program collects keyword
-program creates search query
-program requests info
-program collects info
+*user enters keyword
+*user clicks submit
+*program collects keyword
+*program creates search query
+*program requests info
+*program collects info
 program creates html w/ img and link (repeatedly)
 user clicks on chosen image, goes to recipe site
 user enters new search
